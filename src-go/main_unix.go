82b66a099e5880ca9eb0fed1aeb60753b77f1415
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2025-11-20 22:45:59 +0800

    Update main_unix.go

--- src-go/main_unix.go
+++ src-go/main_unix.go
@@ -6,6 +6,6 @@ package main
 import "github.com/metacubex/mihomo/log"
 
 func runPowerShellScript() {
-	// 在 Linux 和 macOS 上不执行任何操作
+	// 在 Linux 和 macOS 上只记录日志，不执行任何操作
 	log.Debugln("Skipping PowerShell script on non-Windows platform")
 }
